64c99be0f580527b3e8b5173fa4aafe6e42afd5e
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-04-11 15:29:32 +0530

    initial server setup

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-const express=require('express')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+
+//dotenv config
+dotenv.config();
+
+//rest app
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan());
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello welcome to my world");
+});
+
+//PORT number
+const PORT = 8000;
+
+//Listening the PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
